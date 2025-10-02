a030a5b969d320e3606fa6d5ee4623eb52d2755b
Riki PurPur <rikipurpur98@gmail.com>
2025-10-02 22:26:44 +0000

    UI menggunakan Swagger

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
+import "swagger-ui-react/swagger-ui.css"
 
 export const metadata: Metadata = {
   title: 'NirKyy API Explorer',
